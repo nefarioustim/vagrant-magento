c455e68ef2f0d8ac8ca9acc881c4f99a457bd8d2
Tim Huegdon <tim@nefariousdesigns.co.uk>
2013-09-29 19:48:17 +0100

    Add PPA for PHP for newer version, and also install php-cli and php-pear.

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,7 +1,37 @@
+class ondrejppa {
+    include apt
+
+    apt::pparepo { "ondrej/php5":
+        apt_key => "E5267A6C",
+        ensure => present
+    }
+}
+
 class php {
+    require ondrejppa
+
     package { [
             "php5-fpm",
+            "php5-cli",
+            "php-pear",
         ]:
         ensure => latest,
+        require => Class['ondrejppa'],
+    }
+}
+
+class php::composer {
+    exec { "getcomposer":
+        command => "curl -sS https://getcomposer.org/installer | php",
+        user => "root",
+        require => [
+            Class['php'],
+            Package["curl"]
+        ],
+    }
+    exec { "movecomposer":
+        command => "mv composer.phar /usr/local/bin/composer",
+        user => "root",
+        require => Exec["getcomposer"],
     }
 }
