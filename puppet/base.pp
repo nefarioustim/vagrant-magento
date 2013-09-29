2a69701cdf7e2bd0459fc66fcac853862fe766ad
Tim Huegdon <tim@nefariousdesigns.co.uk>
2013-09-29 10:16:53 +0100

    Add basic rules to base manifest and include host rename module.

---
+++ puppet/base.pp
@@ -0,0 +1,36 @@
+stage { "pre": before => Stage["main"] }
+
+Exec {
+    path => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
+}
+
+class devbox {
+    rename { "magento.nefariousdesigns.co.uk": }
+    exec { "aptupdate":
+        command => "aptitude update --quiet --assume-yes",
+        user => "root",
+        timeout => 0,
+        before => Package["build-essential"]
+    }
+    group { "puppet":
+        ensure => present,
+    }
+    package { "build-essential":
+        ensure => latest,
+        before => Package["vim"],
+    }
+    package { [
+            "tmux",
+            "vim",
+            "git",
+            "git-flow",
+            "aptitude"
+        ]:
+        ensure => latest,
+    }
+    package { "memcached":
+        ensure => latest
+    }
+}
+
+class { "devbox": stage => pre }
