f1256a3f6a3f8cc0ec503400f01739f6d63521f8
Tim Huegdon <tim@nefariousdesigns.co.uk>
2013-10-06 08:25:31 +0100

    Add vagrant specific alterations to user profile.

---
+++ puppet/modules/vagrant-user/manifests/init.pp
@@ -0,0 +1,50 @@
+class vagrant-user {
+    user { "vagrant":
+        ensure => present,
+        groups => [ "sudo" ]
+    }
+    user { "www-user":
+        ensure => present,
+        groups => [ "sudo" ]
+    }
+    file { "/home/vagrant/.bashrc":
+        ensure => present,
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:///modules/user/bashrc",
+        require => User["vagrant"]
+    }
+    file { "/home/vagrant/.inputrc":
+        ensure => present,
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:///modules/user/inputrc",
+        require => User["vagrant"]
+    }
+    file { "/home/vagrant/.tmux.conf":
+        ensure => present,
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:///modules/user/tmux_conf",
+        require => User["vagrant"]
+    }
+    file { "/home/vagrant/.environment":
+        ensure => present,
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:///modules/user/environment",
+        require => User["vagrant"]
+    }
+    file { "/home/vagrant/.bash_aliases":
+        ensure => present,
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 644,
+        source => "puppet:///modules/user/bash_aliases",
+        require => User["vagrant"]
+    }
+}
